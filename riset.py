a514548e433b9505a4cdd50504e1f3d72c2ecbf4
Badrull <badrl.xrpl2017smkrus@gmail.com>
2024-02-03 14:59:51 +0700

    +

--- riset.py
+++ riset.py
@@ -1,4 +1,8 @@
+import helper
+helper.db_update("receiver", {
+    "RECEIVER_DATE" : None,
+    "RECEIVER_MESSAGE" : None,
+    "RECEIVER_RESPONSE" : None,
+},f"RECEIVER_WA IN ('6281215992673', '6281348457600', '6282242023609')")
 
-import helper, env
-
-helper.send_wa_multipleSendImage()
+print("ok")
